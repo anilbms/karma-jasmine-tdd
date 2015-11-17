679e27d7f9a1887995342c2e55d32639b31a8734
anilbms <anilbms75@gmail.com>
2015-11-17 10:09:09 +0530

    time converter 12hr AM/PM format to 24hr format.

--- specs/timeConverter.spec.js
+++ specs/timeConverter.spec.js
@@ -1,6 +1,15 @@
-
-describe('Hello world describe', function() {
-    it('method test1', function() {
-        expect(timeConverter()).toBe(true);
+// Time converter  from 12 hour AM / PM convention to 24 hour representation
+describe('Time converter', function() {
+    it('should convert 12hr AM format 24hr format', function() {
+        expect(timeConverter('00:00:00AM')).toBe('00:00:00');
+        expect(timeConverter('10:00:00AM')).toBe('10:00:00');
+        expect(timeConverter('10:00:59AM')).toBe('10:00:59');
+        expect(timeConverter('12:00:00AM')).toBe('12:00:00');
+    });
+    it('should convert 12hr PM format 24hr format', function() {
+        expect(timeConverter('00:00:00PM')).toBe('12:00:00');
+        expect(timeConverter('01:00:00PM')).toBe('13:00:00');
+        expect(timeConverter('11:00:59PM')).toBe('23:00:59');
+        expect(timeConverter('07:00:59PM')).toBe('19:00:59');
     });
 });
--- tests/timeConverter.js
+++ tests/timeConverter.js
@@ -1,3 +1,11 @@
  function timeConverter (input) {
-    return true;
+ 	var  timeArray = input.split(':');
+
+ 	if (input.indexOf('AM')> -1) {
+ 		return input.slice(0, -2);
+ 	} else {
+ 		timeArray[0] = parseInt(timeArray[0]) + 12;
+
+ 		return timeArray.join(':').slice(0, -2);
+ 	}
 }

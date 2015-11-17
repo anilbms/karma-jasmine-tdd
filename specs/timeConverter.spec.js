ae35d7461e5cd6099eca19e4e6d766f30c3a39b3
anilbms <anilbms75@gmail.com>
2015-11-17 09:50:07 +0530

    hello world first example of unit testing using karma and jasmine

---
+++ specs/timeConverter.spec.js
@@ -0,0 +1,6 @@
+
+describe('Hello world describe', function() {
+    it('method test1', function() {
+        expect(timeConverter()).toBe(true);
+    });
+});
---
+++ timeConverter.js
@@ -0,0 +1,3 @@
+function  timeConverter (input) {
+	return true;
+}
